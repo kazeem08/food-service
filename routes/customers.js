269a7ba1193dc45d1b3661e75c322cc3209faa42
kazeem <kazeem0825@gmail.com>
2019-04-10 19:21:07 +0100

    joi validation added for customer model

--- routes/customers.js
+++ routes/customers.js
@@ -1,10 +1,15 @@
 import express from "express";
+import { Customer, validateCustomer as validate } from "../models/customer";
 
 const router = express.Router();
 
+router.get("/", async (req, res) => {
+  const customers = await Customer.find().sort("name");
+  res.send(customers);
+});
 router.post("/", async (req, res) => {
-  // const { error } = validate(req.body);
-  // if (error) return res.status(400).send(error.details[0].message);
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
   const customer = new Customer({
     name: req.body.name,
     email: req.body.email,
@@ -16,4 +21,4 @@ router.post("/", async (req, res) => {
   res.send(customer);
 });
 
-export {};
+export { router };
