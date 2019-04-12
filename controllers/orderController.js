b6d47ac73e9f5633b91c99dc57fe9c1b29d8c85c
kazeem <kazeem0825@gmail.com>
2019-04-12 21:23:59 +0100

    order controller added

---
+++ controllers/orderController.js
@@ -0,0 +1,82 @@
+import { Order, validateOrder as validate } from "../models/order";
+import { Food } from "../models/food";
+import { Customer } from "../models/customer";
+
+const routeController = {};
+
+routeController.get = async (req, res) => {
+  const order = await Order.find().sort("name");
+  res.send(order);
+};
+
+routeController.getById = async (req, res) => {
+  const order = await Order.findById(req.params.id);
+  res.send(order);
+};
+
+routeController.post = async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const customer = await Customer.findById(req.body.customerId);
+
+  const foodCollection = [];
+
+  for (let foodItem of req.body.food) {
+    const food = await Food.findById(foodItem.id);
+    foodCollection.push({
+      _id: food._id,
+      name: food.name,
+      price: food.price,
+      quantity: foodItem.quantity
+    });
+  }
+
+  const order = new Order({
+    customer: {
+      _id: customer._id,
+      name: customer.name,
+      phone: customer.phone
+    },
+    food: foodCollection
+  });
+
+  await order.save();
+  res.send(order);
+};
+
+routeController.put = async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const customer = await Customer.findById(req.body.customerId);
+
+  const foodCollection = [];
+
+  for (let foodItem of req.body.food) {
+    const food = await Food.findById(foodItem.id);
+    foodCollection.push({
+      _id: food._id,
+      name: food.name,
+      quantity: foodItem.quantity
+    });
+  }
+
+  const order = await Order.findByIdAndUpdate(
+    req.params.id,
+    {
+      customer: {
+        _id: customer._id,
+        name: customer.name,
+        phone: customer.phone
+      },
+      food: foodCollection,
+      status: req.body.status
+    },
+    { new: true }
+  );
+
+  res.send(order);
+};
+
+export { routeController };
