2fcc84c062b321990d82df67b43923170779d829
kazeem <kazeem0825@gmail.com>
2019-04-12 19:49:33 +0100

    userController added

---
+++ controllers/userController.js
@@ -0,0 +1,34 @@
+import { User, validateUser as validate } from "../models/user";
+import _ from "lodash";
+import bcrypt from "bcrypt";
+
+const controller = {};
+
+controller.get = async (req, res) => {
+  const users = await User.find();
+  res.send(users);
+};
+
+controller.getbyId = async (req, res) => {
+  const user = await User.findById(req.params.id);
+  res.send(user);
+};
+
+controller.post = async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({ email: req.body.email });
+  if (user) return res.status(400).send("User already registered");
+
+  user = new User(_.pick(req.body, ["name", "email", "password", "isAdmin"]));
+
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+
+  await user.save();
+
+  res.send(_.pick(user, ["name", "email", "phone", "isAdmin"]));
+};
+
+export { controller };
