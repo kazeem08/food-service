3eb2ef66d614907a26bc795affa8fb7309a6437c
kazeem <kazeem0825@gmail.com>
2019-04-13 20:34:59 +0100

    unit test added for jwt token generation

--- startup/db.js
+++ startup/db.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
+import config from "config";
+
+const db = config.get("db");
 
 export default mongoose
-  .connect("mongodb://localhost/food-service", {
+  .connect(db, {
     useNewUrlParser: true,
     useCreateIndex: true
   })
-  .then(() => console.log("connected to MongoDb...."));
+  .then(() => console.log(`connected to ${db}....`));
